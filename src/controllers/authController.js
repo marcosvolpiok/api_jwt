90336ea0d4323f315a3ccd5ec0ec05cc7cad7669
Marcos Volpi <marcos.volpi@BP4.com>
2020-11-29 12:31:37 -0300

    Add auth

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,36 +3,29 @@ class authController{
         this.jwt=require('jsonwebtoken');
         this.config = require('../config/config');
       }
-    
-    /*
-    constructor(messageService) {
-      this.messageService=messageService;
-    }
-  
-    list = async (req, res) => { 
-      try{
-        const messages=await this.messageService.list();
-        res.json(messages);
-      }catch(e){
-        res.status(500).json({message: e.message})
-      }
-    }
-    */
 
    login = async (req, res) => { 
     try{
-        const payload = {
-            check:  true,
-            user: 'test'
-        };
+        if(req.body.user=='demo' && req.body.pwd=='demo'){
+            const payload = {
+                check:  true,
+                user: 'test'
+            };
 
-        const token = this.jwt.sign(payload, this.config.key, {
-            expiresIn: 1440
-        });
-        res.json({
-            mensaje: 'Authentication successful',
-            token: token
-        });
+            const token = this.jwt.sign(payload, this.config.key, {
+                expiresIn: 1440
+            });
+            res.json({
+                mensaje: 'Authentication successful',
+                token: token
+            });
+        }else{
+            console.log('auth failed');
+
+            res.status(403).send({
+                mensaje: 'Authentication failed'
+            });
+        }
     }catch(e){
       res.status(500).json({message: e.message})
     }
